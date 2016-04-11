6c13dc59e7bdedb9fd4d2b5a6f8f2d094aa4be4d
Alex <AlexTheProgrammerMail@gmail.com>
2016-04-10 17:44:35 -0700

    Initial commit. Edmunds Viewer form added

---
+++ src/forms/EdmundsViewer.java
@@ -0,0 +1,14 @@
+package forms;
+
+import javax.swing.*;
+
+/**
+ * Created by Alex on 4/10/16.
+ */
+public class EdmundsViewer {
+    private JComboBox makeComboBox;
+    private JComboBox modelComboBox;
+    private JComboBox yearComboBox;
+    private JComboBox trimComboBox;
+    private JProgressBar progressBar1;
+}
